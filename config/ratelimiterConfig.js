1f9201c3bd7ea05a628c33785cd1c8c7937967eb
Inoland <contact@nobandegan.com>
2024-10-24 13:02:08 +0400

    Update ratelimiterConfig.js

--- config/ratelimiterConfig.js
+++ config/ratelimiterConfig.js
@@ -1,8 +1,11 @@
 import rateLimit from 'express-rate-limit';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes window
-    max: 100, // Limit each IP to 100 requests per `windowMs`
+    windowMs: process.env.RATE_LIMIT_TIME * 60 * 1000, // 15 minutes window
+    max: process.env.RATE_LIMIT_MAX, // Limit each IP to 100 requests per `windowMs`
     message: 'Too many requests from this IP, please try again after 15 minutes.',
     headers: true, // Include rate limit info in the response headers
 });
